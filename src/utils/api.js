7e9a8eab766ef60df18211969cc814433e4c8e44
Akshat Sharma <webdevakshat@gmail.com>
2024-10-08 19:53:43 +0530

    correct backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://restaurant-reservation-system-backend-qq29.onrender.com/api',
 });
 
 api.interceptors.request.use(
